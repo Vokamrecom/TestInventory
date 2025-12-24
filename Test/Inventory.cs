11f610801975643506236cc1e483092dfbab21a5
Vokamrecom <ermakovkiril@gmail.com>
2025-12-24 16:38:37 +0300

    create project

---
+++ Test/Inventory.cs
@@ -0,0 +1,18 @@
+﻿namespace Test;
+
+internal class Inventory
+{
+    private readonly List<Item> _items = [];
+    public IReadOnlyList<Item> Items => _items.AsReadOnly();
+
+    public void AddItem(Item item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+        _items.Add(item);
+    }
+
+    public bool RemoveItem(Item item)
+    {
+        return _items.Remove(item);
+    }
+}
---
+++ Test/Item.cs
@@ -0,0 +1,7 @@
+﻿namespace Test;
+
+internal class Item
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+}
